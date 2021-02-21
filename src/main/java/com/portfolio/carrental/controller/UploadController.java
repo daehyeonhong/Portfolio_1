8f63d9e9d72553bb3b8dd3d19b6f03a73eb4dc4f
DaeHyeonHong <fholng@outlook.com>
2021-02-21 22:46:56 +0900

    upload Ajax

--- src/main/java/com/portfolio/carrental/controller/UploadController.java
+++ src/main/java/com/portfolio/carrental/controller/UploadController.java
@@ -1,9 +1,10 @@
 package com.portfolio.carrental.controller;
 
+import java.io.File;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.log4j.Log4j;
 
@@ -12,12 +13,12 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/upload")
 public class UploadController {
 
-	@GetMapping("/uploadForm")
+	@RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
 	public void uploadForm() {
 		log.info("upload form");
 	}
 
-	@PostMapping("/uploadFormAction")
+	@RequestMapping(value = "/uploadFormAction", method = RequestMethod.POST)
 	public void uploadFormPost(MultipartFile[] uploadFile) {
 		log.info("정상 접근");
 		for (MultipartFile multipartFile : uploadFile) {
@@ -27,9 +28,31 @@ public class UploadController {
 		}
 	}
 
-	@GetMapping("/uploadAjax")
-	public void uploadAjax() {
+	@RequestMapping(value = "/uploadAjax", method = RequestMethod.GET)
+	public void uploadAjax(MultipartFile[] uploadFile) {
 		log.info("upload ajax");
+	}
+
+	@RequestMapping(value = "/uploadAjaxAction", method = RequestMethod.POST)
+	public void uploadAjaxPost(MultipartFile[] uploadFiles) {
+		String uploadFolder = "C:\\TEMP\\upload";
+		for (MultipartFile multipartFile : uploadFiles) {
+			log.info("=====================");
+			String uploadFileName = multipartFile.getOriginalFilename();
+			log.info("Upload File Name : " + uploadFileName);
+			log.info("Upload File Size : " + multipartFile.getSize());
+			// IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("Only File Name : " + uploadFileName);
+
+			File saveFile = new File(uploadFolder, uploadFileName);
+
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getStackTrace());
+			}
+		}
 	}
 
 }
--- src/main/webapp/resources/js/upload/uploadAjax.js
+++ src/main/webapp/resources/js/upload/uploadAjax.js
@@ -2,12 +2,24 @@
 /**
  *
  */
-$(document).ready(function () {
-  $("#uploadBtn").on('click', function (event) {
-    let formData = new FormData();
-    let inputFile = $("input[name='uploadFile']");
-    let files = inputFile[0].files;
-    console.log(files);
-  })
+$(document).ready(function() {
+	$("#uploadBtn").on('click', function(event) {
+		var formData = new FormData();
+		var inputFile = $("input[name='uploadFile']");
+		var files = inputFile[0].files;
+		console.log(files);
+		//add fileDate to formData
+		for (var i = 0; i < files.legnth; i++) {
+			formData.append("uploadFile", files[i]);
+		}
 
-})
+		$.ajax({
+			url: '/uploadAjaxAction',
+			processData: false,
+			contentType: false,
+			data: formData,
+			type: 'POST',
+			success: (result) => console.log("Uploaded")
+		});
+	});
+});
